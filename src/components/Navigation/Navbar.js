2ed81012b5a8633b4f780800c6f206fddff68dd6
nitudeka <nitumonideka327@gmail.com>
2018-10-11 15:12:57 +0530

    added register and login routes to navbar

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import Navlink from './Navlink';
 
 const Navbar = (props) => {
+	let navlink = <Navlink route='/signin' link='Signout' />
+	let newTask = <li className='nav__item'>New Task</li>;
+	if (props.location.pathname === '/signin') {
+		newTask = null;
+		navlink = <Navlink route='/register' link='Register' />
+	} else if (props.location.pathname === '/register') {
+		newTask = null;
+		navlink = <Navlink route='/signin' link='Signin' />
+	}
+	
 	return (
 	  <nav className='nav'>
 			<span className='nav__heading'>TODO</span>
 			<ul className='nav__list'>
-				<Navlink route='/signin' link='Signout' />
-				<li className='nav__item'>New Task</li>
+				{ navlink }
+				{ newTask }
 			</ul>
 		</nav>
 	)
 }
 
-export default Navbar;
+export default withRouter(Navbar);
