569df1346365946d69eeaebb140053b9e08881d1
nitudeka <nitumonideka327@gmail.com>
2018-10-16 14:35:00 +0530

    converted Home to class

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,55 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { taskHandler } from '../../store/actions';
 import Svg from '../Svg';
 
-const Home = (props) => {
-  let tasks = <span className='home__warning'>Please start adding tasks!!</span>
-  if (props.tasks.length > 0) {
-    tasks = props.tasks.map((task) => {
-      return (
-        <li className='home__task' key={task}>
-          <span className='task__name'>{task}</span>
-          <div className='home__svg-container'>
-            <Svg onClick={() => props.completeTask(task)} name='check' className='home__svg home__svg--check' />
-            <Svg onClick={() => props.deleteTask(task)} name='cross' className='home__svg home__svg--cross' />
-          </div>
-        </li>
-      )
-    })
-  }
-
-  let taskProgress = <span className='home__warning'>Please start adding tasks!!</span>;
-  if (props.taskProgress.length > 0) {
-    taskProgress = props.taskProgress.map((taskPrgs, i) => {
-      return (
-        <li key={i} className='home__task'>
-          <span className='task__name'>{taskPrgs}</span>
-        </li>
-      )
-    })
+class Home extends Component {
+  componentDidMount () {
+    this.props.getTasks();
   }
   
-  return (
-    <div className='home'>
-      <div className='home__tasks'>
-        <div className='home__header'>
-          <span className='home__heading'>Your tasks</span>
+  render () {
+    let tasks = <span className='home__warning'>Please start adding tasks!!</span>
+    if (this.props.tasks.length > 0) {
+      tasks = this.props.tasks.map((task) => {
+        return (
+          <li className='home__task' key={task}>
+            <span className='task__name'>{task}</span>
+            <div className='home__svg-container'>
+              <Svg onClick={() => this.props.completeTask(task)} name='check' className='home__svg home__svg--check' />
+              <Svg onClick={() => this.props.deleteTask(task)} name='cross' className='home__svg home__svg--cross' />
+            </div>
+          </li>
+        )
+      })
+    }
+
+    let taskProgress = <span className='home__warning'>Please start adding tasks!!</span>;
+    if (this.props.taskProgress.length > 0) {
+      taskProgress = this.props.taskProgress.map((taskPrgs, i) => {
+        return (
+          <li key={i} className='home__task'>
+            <span className='task__name'>{taskPrgs}</span>
+          </li>
+        )
+      })
+    }
+    
+    return (
+      <div className='home'>
+        <div className='home__tasks'>
+          <div className='home__header'>
+            <span className='home__heading'>Your tasks</span>
+          </div>
+          <ol className='home__tasks-container'>
+            {tasks}
+          </ol>
         </div>
-        <ol className='home__tasks-container'>
-          {tasks}
-        </ol>
-      </div>
-      <div className='home__tasks'>
-        <div className='home__header'>
-          <span className='home__heading'>Task Progress</span>
+        <div className='home__tasks'>
+          <div className='home__header'>
+            <span className='home__heading'>Task Progress</span>
+          </div>
+          <ol className='home__tasks-container'>
+            {taskProgress}
+          </ol>
         </div>
-        <ol className='home__tasks-container'>
-          {taskProgress}
-        </ol>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
@@ -58,8 +64,9 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  completeTask: (task) => taskHandler(dispatch, null, task, 'taskCompleted'),
-  deleteTask: (task) => taskHandler(dispatch, null, task, 'deleteTask')
+  getTasks: () => taskHandler(dispatch, null, 'tasks'),
+  completeTask: (task) => taskHandler(dispatch, task, 'taskCompleted'),
+  deleteTask: (task) => taskHandler(dispatch, task, 'deleteTask')
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
