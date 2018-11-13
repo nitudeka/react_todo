1216f200e4edcdf8c93f331e6a1c99c945e181bf
nitudeka <nitumonideka327@gmail.com>
2018-11-13 12:05:25 +0530

    Updates

---
+++ precache-manifest.b1e6070216acf481934a1c863383606c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92900329656bd339c499",
+    "url": "/react_todo/static/js/runtime~main.92900329.js"
+  },
+  {
+    "revision": "f7e0aab312ba46cc38e5",
+    "url": "/react_todo/static/js/main.f7e0aab3.chunk.js"
+  },
+  {
+    "revision": "da62fd406ef6399932b9",
+    "url": "/react_todo/static/js/1.da62fd40.chunk.js"
+  },
+  {
+    "revision": "f7e0aab312ba46cc38e5",
+    "url": "/react_todo/static/css/main.97b5df53.chunk.css"
+  },
+  {
+    "revision": "af6810591e7fe72cacddee84b97d0a6c",
+    "url": "/react_todo/index.html"
+  }
+];
