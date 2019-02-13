6116d4379b89a8e7bedc3b0494255162b29d23ba
nitudeka <nitumonideka327@gmail.com>
2019-02-13 15:13:46 +0530

    completed writing the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,17 @@ import Form from '../form/index';
 const header = (props) => {
   return (
     <div className='header'>
-      <Form />
+      <span className='header__seperator'></span>
+      <div className='header__container'>
+        <div className='header__container--main'>
+          <h2 className='header__heading'>Todo</h2>
+          <span className='header__heading--main'>An app to manage your tasks</span>
+          <span className='header__heading--sub'>Try it or create an account to use it across your devices</span>
+        </div>
+        <div className='header__container--sub'>
+          <Form />
+        </div>
+      </div>
     </div>
   )
 };
