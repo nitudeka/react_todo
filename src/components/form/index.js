f28b042f2c2051982d453bbddb3c61ee3d3705d4
nitudeka <nitumonideka327@gmail.com>
2019-02-14 10:15:26 +0530

    converted form from class to functional component

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { inputChangeHandler } from '../../store/actions';
 import Input from './input';
 
-class Form extends Component {
-  render() {
-    const inputs = Object.keys(this.props.inputs).map((input) => {
-      const inputName = this.props.inputs[input];
-      return <Input onChange={(event) => this.props.inputChangeHandler(event.target.value, input, inputName.rules) } focused={inputName.focused} valid={inputName.valid} key={input} type={inputName.type} placeholder={inputName.placeholder} />
-    });
-    
-    return (
-      <div className='form'>
-        <span className='form__header'>Register</span>
-        <div className='form__container'>
-          { inputs }
-        </div>
-        <button className='form__btn'>I'm in</button>
+const form = (props) => {
+  const inputs = Object.keys(props.inputs).map((input) => {
+    const inputName = props.inputs[input];
+    return <Input onChange={(event) => props.inputChangeHandler(event.target.value, input, inputName.rules) } focused={inputName.focused} valid={inputName.valid} key={input} type={inputName.type} placeholder={inputName.placeholder} />
+  });
+
+  return (
+    <div className='form'>
+      <span className='form__header'>Register</span>
+      <div className='form__container'>
+        { inputs }
       </div>
-    )
-  }
-}
+      <button className='form__btn'>I'm in</button>
+    </div>
+  )
+};
 
 const mapStateToProps = (state) => {
   const { form } = state;
@@ -36,4 +34,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Form);
+export default connect(mapStateToProps, mapDispatchToProps)(form);
