a33e22e807f3bfb93f72fc881a144c0224084612
nitudeka <nitumonideka327@gmail.com>
2019-02-17 08:29:07 +0530

    Updates

---
+++ precache-manifest.56a50e645e7c5cde11be738bdaee6422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "05733d49e981d1de00bffe2d5cc0f0ac",
+    "url": "/react_todo/static/media/home-bg.05733d49.jpg"
+  },
+  {
+    "revision": "92900329656bd339c499",
+    "url": "/react_todo/static/js/runtime~main.92900329.js"
+  },
+  {
+    "revision": "7861b1cca770d582468d",
+    "url": "/react_todo/static/js/main.7861b1cc.chunk.js"
+  },
+  {
+    "revision": "a7e5c3a08334d287cbc5",
+    "url": "/react_todo/static/js/1.a7e5c3a0.chunk.js"
+  },
+  {
+    "revision": "7861b1cca770d582468d",
+    "url": "/react_todo/static/css/main.b3d14b4c.chunk.css"
+  },
+  {
+    "revision": "3bcb275e95133b4cdeeab826c9ab497f",
+    "url": "/react_todo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react_todo/precache-manifest.108b7d6a481716cbdc8fe0e91846e095.js"
+  "/react_todo/precache-manifest.56a50e645e7c5cde11be738bdaee6422.js"
 );
 
 workbox.clientsClaim();
