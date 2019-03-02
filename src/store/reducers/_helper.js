46b24f231c779eb6ee8e466b33acd8b2406a7f4b
nitudeka <nitumonideka327@gmail.com>
2019-03-02 09:46:03 +0530

    added logic to get the dates

--- src/store/reducers/_helper.js
+++ src/store/reducers/_helper.js
@@ -3,34 +3,52 @@
  *
  */
 
+/*
+* returns all the dates in the requested month of the requested year
+* adds few days of the last month and the next months
+* to make the total no. of days 42
+*/
 export const allDatesOfMonth = (month, year) => {
 	let allDatesInLastMonth = [];
 	let allDatesInMonth = [];
 	let allDatesInNextMonth = [];
-	// const noOfDaysInAllMonth = 42;
 
-	month = typeof(month) === 'number' && month >= 1 && month <= 12 ? month : new Date().getMonth() + 1;
+	// validate the inputs or set the defaults
+	month = typeof(month) === 'number' && month >= 1 && month <= 12 ? month : new Date().getMonth() + 2;
 	year = typeof(year) === 'number' ? year : new Date().getFullYear();
 
+	// returns total no. of dates in a given month
 	const tatalNoOfDates = (month, year) => {
 		return new Date(year, month, 0).getDate();
 	};
 
+	// returns the first day of the month (integer between 0 to 6)
 	const firstDay = (month, year) => {
 		return new Date(year, month - 1, 1).getDay() - 1;
 	};
 
+	// returns the last day of the month (integer between 0 to 6)
+	const lastDay = (month, year) => {
+		return 6 - new Date(year, month - 1, tatalNoOfDates(month, year)).getDay();
+	};
+
+	// push the dates of the last month to the lastMonth array
 	for (let i=firstDay(month, year); i>=0; i--) {
 		allDatesInLastMonth.push(tatalNoOfDates(month - 1, year) - i);
 	};
 
+	// push all the dates of the month to the month array
 	for (let i=1; i<=tatalNoOfDates(month, year); i++) {
 		allDatesInMonth.push(i);
 	};
 
-	for (let i=1; i<=tatalNoOfDates(month + 1, year); i++) {
+	// push the dates of the next month to the nextMonth array
+	for (let i=0; i<=lastDay(month, year); i++) {
 		allDatesInNextMonth.push(i);
 	};
 
-	return [].concat(allDatesInLastMonth, allDatesInMonth, allDatesInNextMonth);
+	const allDates = [].concat(allDatesInLastMonth, allDatesInMonth, allDatesInNextMonth);
+
+	// concat all the arrays and return it (total length of the array = 42)
+	return allDates;
 };
